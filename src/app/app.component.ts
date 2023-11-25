145f4bf8ea57fc0febe71700277765c60cec77e2
Ivan <ivanvilargomes@gmail.com>
2023-11-25 20:51:18 +0100

    scials ready style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { IntroMyselfComponent } from "./intro-myself/intro-myself.component";
 import { IconsMailComponent } from "./icons-mail/icons-mail.component";
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, NavbarComponent, IntroMyselfComponent, IconsMailComponent]
+    imports: [CommonModule, RouterOutlet, NavbarComponent, IntroMyselfComponent, IconsMailComponent, AboutMeComponent]
 })
 export class AppComponent {
   title = 'my-app';

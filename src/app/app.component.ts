eb1633691daeb6692a93254791f5fbf8f0482675
Ivan <ivanvilargomes@gmail.com>
2023-11-26 19:14:27 +0100

    contact form styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,14 @@ import { IconsMailComponent } from "./icons-mail/icons-mail.component";
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { ContactFormComponent } from "./contact-form/contact-form.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, NavbarComponent, IntroMyselfComponent, IconsMailComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent]
+    imports: [CommonModule, RouterOutlet, NavbarComponent, IntroMyselfComponent, IconsMailComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactFormComponent]
 })
 export class AppComponent {
   title = 'my-app';
